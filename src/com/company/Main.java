f2acb4cb0071e1545fc3369c1cc36c858acea2d5
s2delta <s.jake9000@gmail.com>
2019-09-23 22:39:11 +0500

    improve yest kosyak sout continue game11 printing anyway

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println ("guess lamp" );
-        for (int i = 0; i < 4; i++) {
+        for (int i = 1; i < 4; i++) {
 
             Scanner s1 =new Scanner (System.in);
             System.out.println ("try number" +""+ i );
@@ -14,7 +14,11 @@ public class Main {
             switch (in){
                 case "lamp":
                     System.out.println ("match" );
-
+                    System.out.println ("continue game? y/n" );
+                    String con1 =s1.nextLine ();
+                    if (con1.equals ("y")) {i= 0;}
+                       else
+                        System.out.println ("end game good job" );
                     i=4;
                     break;
 
@@ -25,10 +29,26 @@ public class Main {
 
                 default:
                     System.out.println ("try left "+ (3-i));
-                    System.out.println ("continue game y/n" );
-                    String con =s1.nextLine ();
-                    if (con.equals ("n"))
-                        i=4;
+                    if (i==3){
+
+                        System.out.println ("continue game y/n" );
+                   String con =s1.nextLine ();
+                   if (con.equals ("n")) {
+                       System.out.println ("game over" );
+                       i=4;
+                   }
+                   else  i = 0;
+                        System.out.println ("continue game11" );
+
+
+                    }
+
+ //                   System.out.println ("continue game y/n" );
+//                    String con =s1.nextLine ();
+//                    if (con.equals ("n"))
+//                        i=4;
+
+
 
 
             }

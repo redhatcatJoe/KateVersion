4b5aa037f8559576542b5423769fc34b73b4a95b
redhatjoe <redhatjoe@gmail.com>
2019-09-23 22:51:25 +0500

    improve yest kosyak sout continue game11 printing anyway

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,7 @@ package com.company;
 
 import java.util.Scanner;
 
-public class Main {
+public class Main {    ///wtf git fffutttt
 
     public static void main(String[] args) {
         System.out.println ("guess lamp" );
